71800db3392ffa2076337732c36fcb9e692103cc
Stew Anoya <stewartanoya@gmail.com>
2021-12-07 15:21:15 -0500

    Create function to handle conditional classes

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
 
-  const dayClass = classNames();
+  const formatSpots = () => {
+    if (!spots) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
+  const dayClass = classNames(
+    "day-list__item",
+    { "day-list__item--selected": selected },
+    { "day-list__item--full": !spots }
+  );
 
   return (
-    <li onClick={() => setDay(name)}>
+    <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light"> {spots} spots remaining </h3>
+      <h3 className="text--light"> {formatSpots()}</h3>
     </li>
   );
 }
