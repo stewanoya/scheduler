0ac6142015b3ec768eadff264cf28131a36946e1
Stew Anoya <stewartanoya@gmail.com>
2021-12-07 14:18:27 -0500

    add props to button for storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
 import "components/Button.scss";
 
 export default function Button(props) {
-  return <></>;
+  let buttonClass = "button";
+
+  const { onClick, disabled, confirm, danger } = props;
+
+  if (confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button disabled={disabled} onClick={onClick} className={buttonClass}>
+      {props.children}
+    </button>
+  );
 }
