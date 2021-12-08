d2a2729cd82a02388b4aacbb53d1ca96d6a0613f
Stew Anoya <stewartanoya@gmail.com>
2021-12-08 18:45:44 -0500

    Fix type for onsave

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 const Appointment = (props) => {
-  const { time } = props;
+  const { time, student, interview } = props;
 
-  const formatAppt = () => {
-    if (!time) {
-      return "No Appointments";
-    } else {
-      return `Appointment at ${time}`;
-    }
-  };
-  return <article className="appointment">{formatAppt()}</article>;
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {props.interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 };
 
 export default Appointment;
