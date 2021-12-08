af0e0ea9c5629d9d0192a0de6c9d4bc042f8a49c
Stew Anoya <stewartanoya@gmail.com>
2021-12-08 14:48:10 -0500

    Refactor clickable story

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -3,14 +3,13 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 const InterviewerListItem = (props) => {
-  console.log(props);
-  const { selected, setInterviewer, name, avatar, id } = props;
+  const { selected, setInterviewer, name, avatar } = props;
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": selected,
   });
 
   return (
-    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img className="interviewers__item-image" src={avatar} alt={name} />
       {selected && name}
     </li>
