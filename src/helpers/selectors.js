7ebb13b2252bb549b38a52a2a7d68e2b6c409a03
Stew Anoya <stewartanoya@gmail.com>
2021-12-09 17:33:03 -0500

    Create helper to filter appointments based on day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter((state) => state.name === day);
+
+  if (filteredDays.length === 0) {
+    return [];
+  }
+
+  const filteredApptsInDay = filteredDays[0].appointments;
+
+  const filteredAppointments = [];
+
+  for (const appt of filteredApptsInDay) {
+    filteredAppointments.push(state.appointments[appt]);
+  }
+
+  return filteredAppointments;
+}
