c46d2a92007cd33d5d55694c7ffbfbd44ed937b7
Stew Anoya <stewartanoya@gmail.com>
2021-12-10 16:38:55 -0500

    Add conditional show and empty useVisual

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,17 +3,36 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisiualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 const Appointment = (props) => {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
   const { time, interview } = props;
+
+  const { mode, transition, back } = useVisiualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {props.interview ? (
-        <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => console.log("SAVED!")}
+          onCancel={() => back()}
+        />
       )}
     </article>
   );
