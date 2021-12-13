b2e714248b899b167c0d753e44bfc67f927525b2
Stew Anoya <stewartanoya@gmail.com>
2021-12-13 13:26:39 -0500

    refactor save to include saving and transition to show

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,32 +5,41 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisiualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
 
 const Appointment = (props) => {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
-  const { time, interview } = props;
+  const { time, interview, bookInterview } = props;
 
   const { mode, transition, back } = useVisiualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  const save = (name, interviewer) => {
+    transition(SAVING);
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    bookInterview(props.id, interview).then(() => transition(SHOW));
+  };
 
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show
-          student={interview.student}
-          interviewer={props.interview.interviewer}
-        />
+        <Show student={interview.student} interviewer={interview.interviewer} />
       )}
+      {mode === SAVING && <Status message={mode} />}
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onSave={() => console.log("SAVED!")}
+          onSave={save}
           onCancel={() => back()}
         />
       )}
