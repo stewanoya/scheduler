eaeb0a2029b0d479d0c665a0b096cca57b31b576
Stew Anoya <stewartanoya@gmail.com>
2021-12-13 20:46:31 -0500

    add check for interviewer

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Show = (props) => {
   const { student, interviewer, onEdit, onDelete } = props;
+
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer.name}</h3>
+          <h3 className="text--regular">
+            {interviewer ? interviewer.name : null}
+          </h3>
         </section>
       </section>
       <section className="appointment__card-right">
